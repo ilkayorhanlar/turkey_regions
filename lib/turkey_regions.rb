78843db83e5416e33efd8a9b73de1929049515de
Ilkay Orhanlar <ilkayorhanlar@gmail.com>
2019-12-09 14:55:55 +0300

    Update turkey_regions.rb

--- lib/turkey_regions.rb
+++ lib/turkey_regions.rb
@@ -1,11 +1,18 @@
 class TurkeyRegions
   def self.regions
     items = []
-    region_source = YAML.load_file('/data/regions_and_cities.yaml')
+    region_source = YAML.load_file(File.join((File.dirname __dir__), 'lib', 'data', 'regions_and_cities.yaml'))
     region_source.each do |region|
       items << { id: region[0], name: region[1]['translations'][I18n.locale.to_s.split('-')[0]] }
     end
 
     items
+  end
+
+  def self.cities_by_region(region)
+    region_source = YAML.load_file(File.join((File.dirname __dir__), 'lib', 'data', 'regions_and_cities.yaml'))
+    founded_region = region_source.select { |r| r == region }
+
+    founded_region[region]['subdivision_codes'].map(&:to_s)
   end
 end
