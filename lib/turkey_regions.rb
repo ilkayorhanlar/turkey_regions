613dc4cbe634bc137a7c178271c523600c966fb1
Ilkay Orhanlar <ilkayorhanlar@gmail.com>
2019-12-11 12:10:52 +0300

    Update turkey_regions.rb

--- lib/turkey_regions.rb
+++ lib/turkey_regions.rb
@@ -10,9 +10,13 @@ class TurkeyRegions
   end
 
   def self.cities_by_region(region)
-    region_source = YAML.load_file(File.join((File.dirname __dir__), 'lib', 'data', 'regions_and_cities.yaml'))
-    founded_region = region_source.select { |r| r == region }
-
-    founded_region[region]['subdivision_codes'].map(&:to_s)
+    begin
+      region_source = YAML.load_file(File.join((File.dirname __dir__), 'lib', 'data', 'regions_and_cities.yaml'))
+      founded_region = region_source.select { |r| r == region }
+  
+      founded_region[region]['subdivision_codes'].map(&:to_s)
+    rescue
+      raise StandardError.new('given region could not be found')
+    end
   end
 end
