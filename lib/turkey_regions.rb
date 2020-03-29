95da5abfc72327ea69767f1e3c0a8211ed915c08
İlkay Orhanlar <ilkayorhanlar@gmail.com>
2020-03-29 15:21:41 +0300

    added city names to source file and refactoring.

--- lib/turkey_regions.rb
+++ lib/turkey_regions.rb
@@ -1,20 +1,24 @@
 class TurkeyRegions
   def self.regions
     items = []
-    region_source = YAML.load_file(File.join((File.dirname __dir__), 'lib', 'data', 'regions_and_cities.yaml'))
-    region_source.each do |region|
+    source = YAML.load_file(File.join((File.dirname __dir__), 'lib', 'data', 'regions_and_cities.yaml'))
+    source.each do |region|
       items << { id: region[0], name: region[1]['translations'][I18n.locale.to_s.split('-')[0]] }
     end
 
     items
   end
 
-  def self.cities_by_region(region)
+  def self.cities_by_region(region, include_city_name = false)
     begin
-      region_source = YAML.load_file(File.join((File.dirname __dir__), 'lib', 'data', 'regions_and_cities.yaml'))
-      founded_region = region_source.select { |r| r == region }
-  
-      founded_region[region]['subdivision_codes'].map(&:to_s)
+      source = YAML.load_file(File.join((File.dirname __dir__), 'lib', 'data', 'regions_and_cities.yaml'))
+      matched_region = source.select { |r| r == region }
+      
+      if include_city_name
+        matched_region[region]['subdivisions']
+      else
+        matched_region[region]['subdivisions'].map { |s| s['code'] }
+      end
     rescue
       raise StandardError.new('given region could not be found')
     end
